8bebbf82ca9996fa0369826f7bcadef5ca86b99c
flatironschool <danliu277@gmail.com>
2020-11-19 16:16:56 -0500

    Add css for buttons

--- src/containers/carousel.js
+++ src/containers/carousel.js
@@ -18,26 +18,42 @@ const Carousel = () => {
     }
 
     const prevPage = () => {
-        if(page > 0) 
+        if (page > 0)
             setPage(page - 1)
     }
 
     const nextPage = () => {
-        if(page < (items.length / 3) - 1)
+        if (page < (items.length / 3) - 1)
             setPage(page + 1)
+    }
+
+    const prevPageClass = () => {
+        let name = "page-button"
+        if (page > 0)
+            name += " active-button"
+        return name
+    }
+
+    const nextPageClass = () => {
+        let name = "page-button"
+        if (page < (items.length / 3) - 1)
+            name += " active-button"
+        return name
     }
 
     return (
         <div>
-            <h4 className="carousel-title">SHOP BESTSELLERS</h4>
-            <div className="page-toggle">
-                <button onClick={prevPage}>&lt;</button>
-                <button onClick={nextPage}>&gt;</button>
+            <div>
+                <h4 className="carousel-title">SHOP BESTSELLERS</h4>
+                <div className="page-toggle">
+                    <button className={prevPageClass()} onClick={prevPage}>&lt;</button>
+                    <button className={nextPageClass()} onClick={nextPage}>&gt;</button>
+                </div>
             </div>
             <div className="carousel">
                 {getItems()}
             </div>
-            <button>SHOP ALL BESTSELLERS</button>
+            <button className="shop-all-button">SHOP ALL BESTSELLERS</button>
         </div>
     )
 }
